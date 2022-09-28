86fe235e42a3a19cda54e8640301f16da51db990
jasonbirra <jose.celisv@hotmail.com>
2022-09-27 22:40:39 -0300

    actualizacion, codigo limpio

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,15 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 import { GoogleMap } from '@angular/google-maps';
 
-interface Local {
-  position: {
-    lat: number;
-    lng: number;
-  };
-  nombre: string;
-  image: string;
-  text: string;
-}
+
 
 @Component({
   selector: 'app-tab1',
