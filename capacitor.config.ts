482b1b65f9a0672aaec456c4e31ff4a7de05209e
jasonbirra <jose.celisv@hotmail.com>
2023-05-23 04:03:41 -0400

    agregando splash, iconos y cambio de nombre

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,9 +2,28 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'menuinferior',
+  appName: 'CeliakOS v2.0',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      backgroundColor: "#00000000",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true
+    },
+  },
+
 };
 
 export default config;
